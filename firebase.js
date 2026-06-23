edc355b59b20dc8fe2719b46eb78e7640facbb90
protichenoch1 <protichenoch1@gmail.com>
2026-06-23 13:05:30 +0300

    Initialize Firebase app with configuration

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCI7mo2yUPy9OK54xqASV4eiKaBsa6K_Ro",
+  authDomain: "axum-power.firebaseapp.com",
+  projectId: "axum-power",
+  storageBucket: "axum-power.firebasestorage.app",
+  messagingSenderId: "266799442864",
+  appId: "1:266799442864:web:b425676d77f29fa4428475"
+};
+
+export const app = initializeApp(firebaseConfig);
